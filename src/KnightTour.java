5355b9bdaa35e72851df59d8642cd9989ae01b25
dhoaibao <dhbao2505@gmail.com>
2024-03-13 16:57:26 +0700

    Knight Tour

--- src/KnightTour.java
+++ src/KnightTour.java
@@ -1,84 +1,215 @@
-class KnightTour {
-    static int N = 8;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
-    /* A utility function to check if i,j are
-    valid indexes for N*N chessboard */
-    static boolean isSafe(int x, int y, int[][] sol) {
-        return (x >= 0 && x < N && y >= 0 && y < N
-                && sol[x][y] == -1);
+public class KnightTour extends JFrame implements ActionListener {
+    int N;
+    int[] xMove = {2, 1, -1, -2, -2, -1, 1, 2};
+    int[] yMove = {1, 2, 2, 1, -1, -2, -2, -1};
+    int[][] board;
+    JButton[][] squares;
+    JMenuBar menuBar;
+    JMenu Menu;
+    JMenuItem continueItem, newItem;
+
+    KnightTour() {
+        N = 0;
+        board = new int[N][N];
+        squares = new JButton[N][N];
     }
 
-    /* A utility function to print solution
-    matrix sol[N][N] */
-    static void printSolution(int[][] sol) {
-        for (int x = 0; x < N; x++) {
-            for (int y = 0; y < N; y++)
-                System.out.print(sol[x][y] + " ");
+    void readFile() {
+        try (Scanner myReader = new Scanner(new File("./src/txt/KnightTour.txt"))) {
+            N = Integer.parseInt(myReader.nextLine());
+            board = new int[N][N];
+            for (int i = 0; myReader.hasNextLine(); i++) {
+                String[] tokens = myReader.nextLine().split(" ");
+                for (int j = 0; j < N; j++) board[i][j] = Integer.parseInt(tokens[j]);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Lỗi đọc file!");
+        }
+    }
+
+    void writeFile() {
+        try (FileWriter myWriter = new FileWriter("./src/txt/KnightTour.txt")) {
+            myWriter.write(N + "\n");
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    myWriter.write(board[i][j] + " ");
+                }
+                myWriter.write("\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Lỗi ghi file!");
+        }
+    }
+
+    void printSolution(int[][] board) {
+        System.out.println(N);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+                System.out.print(board[i][j] + " ");
             System.out.println();
         }
     }
 
-    /* This function solves the Knight Tour problem
-    using Backtracking. This function mainly
-    uses solveKTUtil() to solve the problem. It
-    returns false if no complete tour is possible,
-    otherwise return true and prints the tour.
-    Please note that there may be more than one
-    solutions, this function prints one of the
-    feasible solutions. */
-    static boolean solveKT() {
-        int[][] sol = new int[8][8];
-
-        /* Initialization of solution matrix */
-        for (int x = 0; x < N; x++)
-            for (int y = 0; y < N; y++)
-                sol[x][y] = -1;
-
-		/* xMove[] and yMove[] define next move of Knight.
-		xMove[] is for next value of x coordinate
-		yMove[] is for next value of y coordinate */
-        int[] xMove = {2, 1, -1, -2, -2, -1, 1, 2};
-        int[] yMove = {1, 2, 2, 1, -1, -2, -2, -1};
-
-        // Since the Knight is initially at the first block
-        sol[0][0] = 0;
-
-		/* Start from 0,0 and explore all tours using
-		solveKTUtil() */
-        if (!solveKTUtil(0, 0, 1, sol, xMove, yMove)) {
-            System.out.println("Solution does not exist");
-            return false;
-        } else
-            printSolution(sol);
-
-        return true;
+    boolean isSafe(int x, int y, int[][] board) {
+        return (x >= 0 && x < N && y >= 0 && y < N
+                && board[x][y] == 0);
     }
 
-    /* A recursive utility function to solve Knight
-    Tour problem */
-    static boolean solveKTUtil(int x, int y, int movei,
-                               int[][] sol, int[] xMove,
-                               int[] yMove) {
+    void timeDelay(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    boolean solveKTUtil(int x, int y, int movei,
+                        int[][] board, int[] xMove,
+                        int[] yMove) {
         int k, next_x, next_y;
         if (movei == N * N)
             return true;
 
-		/* Try all next moves from the current coordinate
-			x, y */
         for (k = 0; k < 8; k++) {
             next_x = x + xMove[k];
             next_y = y + yMove[k];
-            if (isSafe(next_x, next_y, sol)) {
-                sol[next_x][next_y] = movei;
+            if (!isSafe(next_x, next_y, board)) continue;
+
+            if (board[next_x][next_y] == 1) {
                 if (solveKTUtil(next_x, next_y, movei + 1,
-                        sol, xMove, yMove))
-                    return true;
-                else
-                    sol[next_x][next_y]
-                            = -1; // backtracking
+                        board, xMove, yMove)) return true;
+            } else {
+                board[next_x][next_y] = 1;
+                updateBoard(next_x, next_y, true);
+                if (solveKTUtil(next_x, next_y, movei + 1,
+                        board, xMove, yMove)) return true;
+                updateBoard(next_x, next_y, false);
             }
         }
-
         return false;
+    }
+
+    void updateBoard(int row, int col, boolean placeKnight) {
+        timeDelay(200);
+        if (placeKnight) {
+            placeKnight(row, col);
+        } else {
+            board[row][col] = 0;
+            squares[row][col].setBackground(Color.RED);
+            timeDelay(200);
+            squares[row][col].setBackground((row + col) % 2 == 0 ? Color.WHITE : Color.BLACK);
+            squares[row][col].setIcon(null);
+        }
+    }
+
+    void solveKT() {
+        if (!solveKTUtil(0, 0, 1,board, xMove, yMove)) {
+            JOptionPane.showMessageDialog(null, "Không tìm được lời giải!");
+            return;
+        }
+        printSolution(board);
+    }
+
+    void createMenu() {
+        menuBar = new JMenuBar();
+        Menu = new JMenu("Menu");
+
+        continueItem = createMenuItem("Tiếp tục");
+        newItem = createMenuItem("Tạo mới");
+
+        Menu.add(continueItem);
+        Menu.add(newItem);
+
+        menuBar.add(Menu);
+        setJMenuBar(menuBar);
+    }
+
+    JMenuItem createMenuItem(String name) {
+        JMenuItem item = new JMenuItem(name);
+        item.addActionListener(this);
+        return item;
+    }
+
+    void createChessBoard() {
+        setTitle("Bàn cờ vua " + N + "x" + N);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(N, N));
+        createMenu();
+
+        squares = new JButton[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                JButton square = new JButton();
+                square.setPreferredSize(new Dimension(75, 75));
+                square.setBackground((i + j) % 2 == 0 ? Color.WHITE : Color.BLACK);
+                square.addActionListener(this);
+                squares[i][j] = square;
+                if (board[i][j] == 1) placeKnight(i, j);
+                add(square);
+            }
+        }
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    void placeKnight(int row, int col) {
+        Icon icon = new ImageIcon(new ImageIcon("./src/icons/horse.png").getImage().getScaledInstance(75, 75, Image.SCALE_SMOOTH));
+        squares[row][col].setIcon(icon);
+    }
+
+    void resetChessBoard() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                squares[i][j].setIcon(null);
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == continueItem) {
+            solveKT();
+//            writeFile();
+        } else if (e.getSource() == newItem) {
+            JOptionPane.showConfirmDialog(null, "Bạn có muốn tạo bàn cờ mới?", "Tạo mới", JOptionPane.YES_NO_OPTION);
+            resetChessBoard();
+            solveKT();
+//            writeFile();
+        } else {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (e.getSource() == squares[i][j]) {
+                        board[i][j] = Math.abs(board[i][j] - 1);
+                        if (board[i][j] == 1) placeKnight(i, j);
+                        else squares[i][j].setIcon(null);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    void start() {
+        readFile();
+        createChessBoard();
+        solveKT();
+    }
+
+    public static void main(String[] args) {
+        KnightTour Knight = new KnightTour();
+        Knight.start();
     }
 }
--- src/NQueen.java
+++ src/NQueen.java
@@ -1,11 +1,13 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class NQueen extends JFrame implements ActionListener {
     int N;
     int[][] board;
@@ -13,30 +15,28 @@ public class NQueen extends JFrame implements ActionListener {
     JMenuBar menuBar;
     JMenu Menu;
     JMenuItem continueItem, newItem;
+
     NQueen() {
         N = 0;
         board = new int[N][N];
         squares = new JButton[N][N];
     }
+
     void readFile() {
-        try {
-            File myObj = new File("./src/txt/NQueen.txt");
-            Scanner myReader = new Scanner(myObj);
+        try (Scanner myReader = new Scanner(new File("./src/txt/NQueen.txt"))) {
             N = Integer.parseInt(myReader.nextLine());
-            int i = 0;
-            while (myReader.hasNextLine()) {
+            board = new int[N][N];
+            for (int i = 0; myReader.hasNextLine(); i++) {
                 String[] tokens = myReader.nextLine().split(" ");
-                for (int j = 0; j < N; j++) board[i][j] = Integer.parseInt(tokens[j]); // Parsing each token as integer
-                i++;
+                for (int j = 0; j < N; j++) board[i][j] = Integer.parseInt(tokens[j]);
             }
-            myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("Lỗi đọc file!");
         }
     }
+
     void writeFile() {
-        try {
-            FileWriter myWriter = new FileWriter("./src/txt/NQueen.txt");
+        try (FileWriter myWriter = new FileWriter("./src/txt/NQueen.txt")) {
             myWriter.write(N + "\n");
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
@@ -44,86 +44,105 @@ public class NQueen extends JFrame implements ActionListener {
                 }
                 myWriter.write("\n");
             }
-            myWriter.close();
         } catch (IOException e) {
             System.out.println("Lỗi ghi file!");
         }
     }
+
     void printSolution(int[][] board) {
         System.out.println(N);
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++)
-                System.out.print(board[i][j]
-                        + " ");
+                System.out.print(board[i][j] + " ");
             System.out.println();
         }
     }
+
     boolean isSafe(int[][] board, int row, int col) {
         int i, j;
+
         for (i = 0; i < N; i++)
             if ((board[i][col] == 1 && i != row) || (board[row][i] == 1 && i != col))
                 return false;
+
         for (i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
             if (board[i][j] == 1)
                 return false;
+
         for (i = row + 1, j = col - 1; j >= 0 && i < N; i++, j--)
             if (board[i][j] == 1)
                 return false;
+
         return true;
     }
+
     void timeDelay(int time) {
         try {
             Thread.sleep(time);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
+
     boolean solveNQUtil(int[][] board, int col) {
-        if (col >= N)
-            return true;
+        if (col >= N) return true;
+
         for (int i = 0; i < N; i++) {
-            if (isSafe(board, i, col)) {
-                if (board[i][col] == 1) {
-                    if (solveNQUtil(board, col + 1))
-                        return true;
-                } else {
-                    board[i][col] = 1;
-                    timeDelay(200);
-                    placeQueen(i, col);
-                    if (solveNQUtil(board, col + 1))
-                        return true;
-                    board[i][col] = 0;
-                    squares[i][col].setBackground(Color.RED);
-                    timeDelay(200);
-                    squares[i][col].setBackground((i + col) % 2 == 0 ? Color.WHITE : Color.BLACK);
-                    squares[i][col].setIcon(null);
-                }
+            if (!isSafe(board, i, col)) continue;
+
+            if (board[i][col] == 1) {
+                if (solveNQUtil(board, col + 1)) return true;
+            } else {
+                board[i][col] = 1;
+                updateBoard(i, col, true);
+                if (solveNQUtil(board, col + 1)) return true;
+                updateBoard(i, col, false);
             }
         }
         return false;
     }
+
+    void updateBoard(int row, int col, boolean placeQueen) {
+        timeDelay(200);
+        if (placeQueen) {
+            placeQueen(row, col);
+        } else {
+            board[row][col] = 0;
+            squares[row][col].setBackground(Color.RED);
+            timeDelay(200);
+            squares[row][col].setBackground((row + col) % 2 == 0 ? Color.WHITE : Color.BLACK);
+            squares[row][col].setIcon(null);
+        }
+    }
+
     void solveNQ() {
         if (!solveNQUtil(board, 0)) {
             JOptionPane.showMessageDialog(null, "Không tìm được lời giải!");
             return;
         }
-
         printSolution(board);
     }
+
     void createMenu() {
         menuBar = new JMenuBar();
         Menu = new JMenu("Menu");
-        newItem = new JMenuItem("New");
-        continueItem = new JMenuItem("Continue");
 
-        newItem.addActionListener(this);
-        continueItem.addActionListener(this);
+        continueItem = createMenuItem("Tiếp tục");
+        newItem = createMenuItem("Tạo mới");
 
         Menu.add(continueItem);
         Menu.add(newItem);
+
         menuBar.add(Menu);
         setJMenuBar(menuBar);
     }
+
+    JMenuItem createMenuItem(String name) {
+        JMenuItem item = new JMenuItem(name);
+        item.addActionListener(this);
+        return item;
+    }
+
     void createChessBoard() {
         setTitle("Bàn cờ vua " + N + "x" + N);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -147,10 +166,12 @@ public class NQueen extends JFrame implements ActionListener {
         setLocationRelativeTo(null);
         setVisible(true);
     }
+
     void placeQueen(int row, int col) {
-        Icon icon = new ImageIcon(new ImageIcon("./src/icons/icon.png").getImage().getScaledInstance(75, 75, Image.SCALE_SMOOTH));
+        Icon icon = new ImageIcon(new ImageIcon("./src/icons/queen.png").getImage().getScaledInstance(75, 75, Image.SCALE_SMOOTH));
         squares[row][col].setIcon(icon);
     }
+
     void resetChessBoard() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -158,15 +179,18 @@ public class NQueen extends JFrame implements ActionListener {
                 board[i][j] = 0;
             }
         }
-        writeFile();
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == continueItem) {
             solveNQ();
+//            writeFile();
         } else if (e.getSource() == newItem) {
+            JOptionPane.showConfirmDialog(null, "Bạn có muốn tạo bàn cờ mới?", "Tạo mới", JOptionPane.YES_NO_OPTION);
             resetChessBoard();
-
+            solveNQ();
+//            writeFile();
         } else {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
@@ -180,13 +204,13 @@ public class NQueen extends JFrame implements ActionListener {
             }
         }
     }
+
     void start() {
-        N = 8;
-        board = new int[N][N];
         readFile();
         createChessBoard();
         solveNQ();
     }
+
     public static void main(String[] args) {
         NQueen Queen = new NQueen();
         Queen.start();
